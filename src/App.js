d044f78bea1002d02b5508d2a61c0a6b8c31d310
polarnight <1041465280@qq.com>
2019-10-06 23:21:07 +0800

    完成viewTab组件

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import PriceList from "./components/PriceList";
+import ViewTab from "./components/ViewTab";
+import { LIST_VIEW, CHART_VIEW } from "./utility";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const items = [
   {
     id: 1,
-    title: ""
+    title: "1",
+    price: 400,
+    date: "2019-9-10",
+    category: {
+      id: "1",
+      iconName: "旅行",
+      type: "outcome"
+    }
   }
 ];
 function App() {
   return (
     <div className="App">
-      <PriceList></PriceList>
+      <PriceList
+        items={items}
+        onDeleteItem={item => {
+          alert(item.id);
+        }}
+        onModifyItem={item => {
+          alert(item.id);
+        }}
+      ></PriceList>
+      <ViewTab activeTab={CHART_VIEW} onTabChange={view=>{console.log(view);
+      }} ></ViewTab>
     </div>
   );
 }
