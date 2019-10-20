f78eb1289f237e529dbd151ca69698dc2f2f4ca0
polarnight <1041465280@qq.com>
2019-10-20 22:19:51 +0800

    修复了加载时的一个bug

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path =require('path')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
+const port = process.env.LEANCLOUD_APP_PORT || 3000
 const root = __dirname +'/build'
 server.use(express.static(root, {maxAge:86400000}))
-server.use(router)
 server.use(middlewares)
 const reactRouterWhiteList =['/create','/edit/:itemId']
 server.get(reactRouterWhiteList,(request,response)=>{
-    response.sendFile(path,resolve(root,'index.html'))
+    response.sendFile(path.resolve(root,'index.html'))
 })
-server.listen(3000,()=>{
+server.use(router)
+server.listen(port,()=>{
     console.log('server is running')
 })
